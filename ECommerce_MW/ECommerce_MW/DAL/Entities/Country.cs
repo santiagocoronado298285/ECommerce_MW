3f3c4a2b4c99829fbcfdea73137210516001158d
Santiago C <scoronado@integrupo.com>
2023-03-20 17:13:45 -0500

    creating entities

---
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ECommerce_MW.DAL.Entities
+{
+    public class Country : Entity
+    {
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es oblilgatorio.")]
+        public string Name { get; set; }
+    }
+}
